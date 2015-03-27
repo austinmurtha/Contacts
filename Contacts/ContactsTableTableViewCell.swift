856b7cfcfa5298f9ebedc728d36d9b60ed8e8465
Austin Murtha <Austin.Murtha@gmail.com>
2015-03-27 11:53:01 -0400

    push intial project files

---
+++ Contacts/ContactsTableTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactsTableTableViewCell.swift
+//  Contacts
+//
+//  Created by Austin Murtha on 3/27/15.
+//  Copyright (c) 2015 AustinMurtha. All rights reserved.
+//
+
+import UIKit
+
+class ContactsTableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
