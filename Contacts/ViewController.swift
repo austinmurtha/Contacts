683984d57d53d9e7809b5a0470ee24f9434b1ba0
Austin Murtha <Austin.Murtha@gmail.com>
2015-03-27 09:49:12 -0400

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Austin Murtha on 3/27/15.
+//  Copyright (c) 2015 AustinMurtha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
